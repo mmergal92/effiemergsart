845841144f4a0919bf94c6058fd5f5d957f2c2dc
Maria Mergal <maria@curedesign.co>
2025-11-10 23:31:47 -0500

    edit

---
+++ my-react-app/src/pages/home.js
@@ -0,0 +1,33 @@
+const Home = () =>{
+
+
+
+  return (
+    <div className="home-page ">
+       
+
+      <main className="home-content fade-in fade-in-delay-2">
+        <h1 className="headline">Header Text
+                </h1>
+
+         
+
+        <section className="main-content" id="main">
+    
+
+          <div className="testimonial-slider-container">
+               <p className="main-content">main content
+                </p>
+            </div>
+
+
+        </section>
+      </main>
+
+
+
+    </div>
+  );
+}
+
+export default Home;
