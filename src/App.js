6085331cad7d53f59acf3f9a9d716eeb0c2eae7b
Maria Mergal <maria@curedesign.co>
2025-11-10 23:48:33 -0500

    edit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './App.css';
 //IMPORT ROUTE and COMPONENTS
-import {Route, Routes, Navigate} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from './pages/home';
 // import PageNotFound from './pages/404page';
 
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path="/home" element={<Navigate to="/" replace />} />
+        {/* <Route path="/home" element={<Navigate to="/" replace />} /> */}
         {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
     </div>
