31d0385a92b446ac53a1c3794cb9e0417deaa405
Maria Mergal <maria@curedesign.co>
2025-11-17 12:19:58 -0500

    edit

--- src/pages/404page.js
+++ src/pages/404page.js
@@ -4,21 +4,23 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className="notfound-page">
-      {/* Header (left-aligned, same as homepage) */}
+      {/* Header (left-aligned, like homepage) */}
       <header className="notfound-header">
         <Link to="/" className="site-title-link">
           <h1 className="headline">Effie Mergs Art</h1>
         </Link>
       </header>
 
-      <main className="notfound-content fade-in fade-in-delay-2">
-        <h1 className="notfound-404">404</h1>
-        <p className="notfound-message">
-          Oops! The page you’re looking for does not exist.
-        </p>
-        <Link to="/" className="inquire-button">
-          Return Home
-        </Link>
+      <main className="notfound-content">
+        <div className="notfound-center">
+          <h1 className="notfound-404">404</h1>
+          <p className="notfound-message">
+            Oops! The page you’re looking for does not exist.
+          </p>
+          <Link to="/" className="inquire-button">
+            Return Home
+          </Link>
+        </div>
       </main>
 
       {/* Footer */}
